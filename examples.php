9a42d44dd8af8a88f2d5de43b7797e1430b8eadf
Irina <irinapushkareva29@gmail.com>
2023-09-11 20:28:09 +0300

    create the process of united and diveded fullname

---
+++ examples.php
@@ -0,0 +1,64 @@
+<?php
+$example_persons_array = [
+    [
+        'fullname' => 'Иванов Иван Иванович',
+        'job' => 'tester',
+    ],
+    [
+        'fullname' => 'Степанова Наталья Степановна',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Пащенко Владимир Александрович',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Громов Александр Иванович',
+        'job' => 'fullstack-developer',
+    ],
+    [
+        'fullname' => 'Славин Семён Сергеевич',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Цой Владимир Антонович',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Быстрая Юлия Сергеевна',
+        'job' => 'PR-manager',
+    ],
+    [
+        'fullname' => 'Шматко Антонина Сергеевна',
+        'job' => 'HR-manager',
+    ],
+    [
+        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Бардо Жаклин Фёдоровна',
+        'job' => 'android-developer',
+    ],
+    [
+        'fullname' => 'Шварцнегер Арнольд Густавович',
+        'job' => 'babysitter',
+    ],
+];
+
+for ($i = 0; $i <= 10; $i++) {
+    $divided_fullname = explode (" ",$example_persons_array[$i]['fullname']);
+    $new_array = [
+        'surname' => $divided_fullname[0],
+        'name' => $divided_fullname[1],
+        'patronomyc' => $divided_fullname[2],
+    ];
+    print_r ($new_array);
+
+    $united_fullname = $new_array['surname'].' '.$new_array['name'].' '.$new_array['patronomyc'];
+    print_r($united_fullname);
+
+}
+
+
+
